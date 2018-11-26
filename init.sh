4e73038b1764e06824fd5cf1e4ba022608da4320
Valter Gådin <valter94@live.se>
2018-11-26 13:30:49 +0100

    zsh + removed hidden

--- init.sh
+++ init.sh
@@ -17,3 +17,23 @@ else
         echo Copying vim folders + config
         cp vimrc $HOME/.vimrc
 fi
+
+#Oh my zsh
+if [ -e "$HOME/.oh-my-zsh/" ]
+then 
+        echo Oh-my-zsh already exist
+else
+        wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+fi
+
+#zsh
+if [ -e "$HOME/.zshrc" ]
+then 
+        echo zsh config or folder already exists!
+else
+        echo Copying zsh folders + config
+        cp zshrc $HOME/.zshrc
+        source $HOME/.zshrc
+fi
+
+
