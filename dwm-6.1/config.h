9975e4e813a0826d55628100042c0cfd3a90cfba
Valter Gådin <valter94@live.se>
2018-12-13 12:05:30 +0100

    asdasd

--- dwm-6.1/config.h
+++ dwm-6.1/config.h
@@ -2,26 +2,26 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=12"
+	"Literation Mono Powerline:size=14"
 };
-static const char dmenufont[]       = "monospace:size=12";
+static const char dmenufont[]       = "Literation Mono Powerline:size=14";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
 static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int extrabar           = 1;        /* 0 means no extra bar */
 
 /* Useless Gap*/
-static const unsigned int gappx     = 6;        /* gap pixel between windows */
+static const unsigned int gappx     = 5;        /* gap pixel between windows */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -59,7 +59,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
-
+static const char *day[] = {"echo 900 > /sys/class/backlight/intel_backlight/brightness"
+                            "&& xgamma -s eDP-1 -gamma 1"};
+static const char *night[] = {"echo 100 > /sys/class/backlight/intel_backlight/brightness"
+                            "&& xgamma -s eDP-1 -rgamma 1 -ggamma 0.7  -bgamma 0.5"};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -95,6 +98,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    /* Own */
+	{ MODKEY,                       XK_q,      spawn,          {.v = day} },
+	{ MODKEY,                       XK_w,      spawn,          {.v = night} },
 };
 
 /* button definitions */
