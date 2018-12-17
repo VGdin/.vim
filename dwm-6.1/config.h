6ca763d56afec975bde937121a86fed994bd1ef9
Valter Gådin <valter94@live.se>
2018-12-18 00:49:17 +0100

    colors

--- dwm-6.1/config.h
+++ dwm-6.1/config.h
@@ -12,16 +12,27 @@ static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gap pixel between windows, useless gap */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int extrabar           = 1;        /* 0 means no extra bar */
 
-/* Useless Gap*/
-static const unsigned int gappx     = 5;        /* gap pixel between windows */
+#define NUMCOLORS         4
+static const char colors[NUMCOLORS][MAXCOLORS][8] = {
+    //border   foreground   background
+    { "#c5c8c6", "#c5c8c6", "#1d1f21" },  // normal
+    { "#198844", "#198844", "#1d1f21" },  // selected
+    { "#cc342b", "#cc342b", "#1d1f21" },  // urgent/warning  (black on yellow)
+    { "#fba922", "#fba922", "#1d1f21" },  // error (white on red)
+    { "#3971ed", "#3971ed", "#1d1f21" },  // error (white on red)
+    { "#ff0000", "#ffffff", "#1d1f21" },  // error (white on red)
+
+    // add more here
+};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4"};
+static const char *tags[] = { "web", "term", "dev1", "dev2", "mosh", "spotify"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -44,6 +55,11 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+
+
+
+
+
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
@@ -59,10 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *day[] = {"echo 900 > /sys/class/backlight/intel_backlight/brightness"
-                            "&& xgamma -s eDP-1 -gamma 1"};
-static const char *night[] = {"echo 100 > /sys/class/backlight/intel_backlight/brightness"
-                            "&& xgamma -s eDP-1 -rgamma 1 -ggamma 0.7  -bgamma 0.5"};
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -98,9 +111,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-    /* Own */
-	{ MODKEY,                       XK_q,      spawn,          {.v = day} },
-	{ MODKEY,                       XK_w,      spawn,          {.v = night} },
 };
 
 /* button definitions */
